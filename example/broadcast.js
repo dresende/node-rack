c5c04027a1f9b9b42f92e43b47166781384e279f
Diogo Resende <dresende@thinkdigital.pt>
2011-12-14 22:56:40 +0000

    Change event "worker-message" to "message". Add rack.broadcast()

--- example/broadcast.js
+++ example/broadcast.js
@@ -7,20 +7,22 @@ rack.start();
 function startMaster(rack) {
 	console.log("[%d] master started", rack.pid);
 
-	rack.on("message", function (msg, worker) {
-		console.log("[%d (worker)] -> [%d (master)] %j", worker.pid, rack.pid, msg);
-	});
+	setInterval(function () {
+		rack.broadcast({ msg: "Hello workers!" });
+	}, 2000);
 }
 
 function startWorker(worker) {
 	console.log("[%d] worker started", worker.pid);
 
 	require("http").createServer(function (req, res) {
-		worker.send({ url: req.url });
-
 		console.log("[%d] %s", worker.pid, req.url);
 
 		res.writeHead(200, { "Content-Type": "text/plain" });
 		res.end("Your requested " + req.url);
 	}).listen(1337);
+
+	rack.on("message", function (msg) {
+		console.log("[%d] msg from master: %j", worker.pid, msg);
+	});
 }
